5f667377f5f88db86c0ef39717330b486a07a5e3
Ryan Chung <ryanchuung@gmail.com>
2020-08-06 04:44:32 -0700

    created post create/detail views/url/template

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostCreateView, PostDetailView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-
+    path('create/', PostCreateView.as_view(), name='create-post'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     paginate_by = 10
     ordering = ['-date_posted']
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/create_post.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
