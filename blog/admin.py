ad32801977cca3c2dd29214aedb422d45c1ccdee
Ryan Chung <ryanchuung@gmail.com>
2020-08-06 06:13:17 -0700

    created custom Post admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date_posted')
+    list_filter = ('author',)
+
+    search_fields = ('title',)
+    ordering = ('date_posted',)
+    readonly_fields = ('date_posted',)
+
+    fieldsets = (
+        ('Post', {'fields': ('title', 'content', 'author', 'date_posted')}),
+    )
+    filter_horizontal = ()
+
+admin.site.register(Post, PostAdmin)
