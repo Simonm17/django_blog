bc34278bcc3cc76a2e8826aadec3633b6c6bfe2e
Ryan Chung <ryanchuung@gmail.com>
2020-10-05 03:34:54 -0700

    made secret changes

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('nimda/', admin.site.urls),
     path('', include('blog.urls')),
     path('users/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # for images in development
