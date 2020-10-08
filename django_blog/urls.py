3925dbeb298a8757588611125d91fa2b8b9865c1
Ryan Chung <ryanchuung@gmail.com>
2020-10-07 20:28:03 -0700

    added sentry debug trigger function

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -3,11 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 urlpatterns = [
     path('nimda/', admin.site.urls),
     path('', include('blog.urls')),
     path('users/', include('users.urls')),
+    path('sentry-debug/', trigger_error),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # for images in development
 
 
